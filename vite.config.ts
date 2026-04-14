79ee6cf2e54ec60646fff3f653a89bc8929a4e57
sidougmr7 <goumirisidali@hotmail.com>
2026-04-14 15:24:07 +0100

    remove base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/fiche-equipe-montage",
   server: {
     host: "::",
     port: 8080,
