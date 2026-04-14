c82d66733063c17c2d6f2c526444c2e013a37ac9
sidougmr7 <goumirisidali@hotmail.com>
2026-04-14 14:59:56 +0100

    add base

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter >
+      <BrowserRouter basename="/fiche-equipe-montage">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/fiches" element={<FicheList />} />
